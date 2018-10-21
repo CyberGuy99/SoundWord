302ee63406b02a2e2a3695c9342a413dbddff87b
Rushil Dandamudi <rushilcd@gmail.com>
2018-10-20 20:15:13 -0500

    updated file structure and response for submitting file

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config(object):
     TOP_LEVEL_DIR = os.path.abspath(os.curdir)
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOADS_DEFAULT_DEST = TOP_LEVEL_DIR + '/tmp/static'
-    UPLOADS_DEFAULT_URL = 'http://localhost:5000/static/img/'
+    UPLOADS_DEFAULT_URL = 'http://localhost:5000/static/'
 
--- routes.py
+++ routes.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from config import Config
 from mp3_form import MP3Form
-from upload import upload_file
+import revAPI
+from counts import word_frequencies
+from sentiment import analyze_sentiment
 import templates
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
 ALLOWED_EXTENSIONS = set(['mp3','mp4','wav'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from flask_uploads import UploadSet, AUDIO, configure_uploads
 audios = UploadSet('sounds', AUDIO)
@@ -23,18 +23,51 @@ def home():
     form = MP3Form()
     return render_template("sound.html", form=form)
 
+def get_pol(text):
+    return "LEFT"
+
 @app.route('/uploadMP3', methods=['POST'])
 def post_mp3():
-#   upload_file(request)
-    filename = audios.save(request.files['audio'])
-    url = audios.url(filename)
-    return url + ""
+    form = MP3Form()
+    if form.validate_on_submit():
+        filename = audios.save(request.files['audio'])
+        url = audios.url(filename)
+        doPol = form.pol.data
+        doSent = form.sent.data
+        doFreq = form.sent.data
 
-from flask import send_from_directory
+        full_file = app.config['UPLOADS_DEFAULT_DEST'] + "/sounds/" + filename)
+        print(full_file)
+        text_format = revAPI.audio_to_text(full_file)
+        full_out = ["pol: {} sent: {} freq: {}".format(doPol,doSent,doFreq)]
+        for log in text_format:
+            output = "Speaker {}: "
+            if doPol:
+                output += "political: {} ".format(get_pol(text_format))
+            if doSent:
+                output += "sentiment: {} ".format(analyze_sentiment(text_format))
+            if doFreq:
+                output += "frequencies: {} ".format(word_frequencies(text_format))
+            full_out.append(output)
+        return '\n'.join(full_out)
+    else:
+        #not working at the moment, need to fix this for UI
+        flash_errors(form)
+        flash("ERROR!")
+    return render_template("sound.html", form=form)
 
+#from flask import send_from_directory
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash("Error in the %s field – %s" % (getattr(form, field).label.text, error),'info')
+
+'''
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config['UPLOADS_DEFAULT_DEST'], filename)
+'''
 
 if __name__ == "__main__":
     app.run()
