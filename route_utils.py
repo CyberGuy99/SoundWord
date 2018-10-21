159d8073b36c8b725680afad69041095c6ef2d3b
Rushil Dandamudi <rushilcd@gmail.com>
2018-10-21 11:30:54 -0500

    refactoring

---
+++ route_utils.py
@@ -0,0 +1,23 @@
+import revAPI
+from counts import word_frequencies
+from sentiment import analyze_sentiment
+
+
+#mock method change later
+def get_pol(text):
+    return "LEFT"
+
+
+def analyze_text_full(path, doPol, doSent, doFreq):
+    text_format = revAPI.audio_to_text(path)
+    full_out = ["pol: {} sent: {} freq: {}".format(doPol,doSent,doFreq)]
+    for log in text_format:
+        output = "Speaker {}: "
+        if doPol:
+            output += "political: {} ".format(get_pol(text_format))
+        if doSent:
+            output += "sentiment: {} ".format(analyze_sentiment(text_format))
+        if doFreq:
+            output += "frequencies: {} ".format(word_frequencies(text_format))
+        full_out.append(output)
+    return full_out
--- routes.py
+++ routes.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, request, flash
 from config import Config
 from mp3_form import MP3Form
-import revAPI
-from counts import word_frequencies
-from sentiment import analyze_sentiment
-import templates
-
-ALLOWED_EXTENSIONS = set(['mp3','mp4','wav'])
+#import route_utils #uncomment this after revAPI complete
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,16 +10,10 @@ from flask_uploads import UploadSet, AUDIO, configure_uploads
 audios = UploadSet('sounds', AUDIO)
 configure_uploads(app, audios)
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route("/")
 def home():
     form = MP3Form()
     return render_template("sound.html", form=form)
-
-def get_pol(text):
-    return "LEFT"
 
 @app.route('/uploadMP3', methods=['POST'])
 def post_mp3():
@@ -36,20 +25,11 @@ def post_mp3():
         doSent = form.sent.data
         doFreq = form.sent.data
 
-        full_file = app.config['UPLOADS_DEFAULT_DEST'] + "/sounds/" + filename)
+        full_file = app.config['UPLOADS_DEFAULT_DEST'] + "/sounds/" + filename
         print(full_file)
-        text_format = revAPI.audio_to_text(full_file)
-        full_out = ["pol: {} sent: {} freq: {}".format(doPol,doSent,doFreq)]
-        for log in text_format:
-            output = "Speaker {}: "
-            if doPol:
-                output += "political: {} ".format(get_pol(text_format))
-            if doSent:
-                output += "sentiment: {} ".format(analyze_sentiment(text_format))
-            if doFreq:
-                output += "frequencies: {} ".format(word_frequencies(text_format))
-            full_out.append(output)
-        return '\n'.join(full_out)
+        return full_file
+        analysis = analyze_text_full(full_file, doPol, doSent, doFreq)
+        return '\n'.join(analysis)
     else:
         #not working at the moment, need to fix this for UI
         flash_errors(form)
